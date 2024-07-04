829639d1c415eed399db659c59312d965541c9e9
Rahul Sharma <rahul.sharma@engineering.digital.dwp.gov.uk>
2024-07-04 18:20:32 +0100

    fixed changes
    GitOrigin-RevId: 20017680d80afc4acace172fdb79f9b00154637a

--- app/routes.js
+++ app/routes.js
@@ -323,81 +323,6 @@ router.post('/sprint13/opt2/engagement-type', function (req, res) {
 })
 
 
-// this is for NINO number for UR round-4 (option-1)
-router.post('/sprint19/opt1/engagement-type', function (req, res) {
-
-  req.session.data['What-type-of-engagement-is-it'] = '';
-  req.session.data['Who-is-the-engagement-with'] = '';
-  req.session.data['Do-you-want-to-complete-the-session'] = '';
-  req.session.data['What-services-have-they-called-about'] = '';
-  req.session.data['esaPayment'] = '';
-  req.session.data['pipPayment'] = '';
-  req.session.data['caPayment'] = '';
-  req.session.data['ca-payment'] = '';
-  req.session.data['esa-payment'] = '';
-  req.session.data['pip-payment'] = '';
-  req.session.data['esa-process-chasing'] = '';
-  req.session.data['pip-process-chasing'] = '';
-  req.session.data['ca-process-chasing'] = '';
-  req.session.data['esa-coc'] = '';
-  req.session.data['pip-coc'] = '';
-  req.session.data['ca-coc'] = '';
-  req.session.data['esa-general'] = '';
-  req.session.data['pip-general'] = '';
-  req.session.data['ca-general'] = '';
-
-  if (req.session.data['national-insurance-number-for-ur4'] == 'QQ123456Q' || req.session.data['national-insurance-number-for-ur4'] == 'qq123456q') {
-    // Send user to next page
-    res.render('sprint19/opt1/engagement-type')
-  } else {
-    var errMsg = "";
-    if (req.session.data['national-insurance-number-for-ur4'] == '' || req.session.data['national-insurance-number-for-ur4'] == undefined) {
-      errMsg = "National Insurance Number is mandatory";
-    } else {
-      errMsg = "Enter a National Insurance number in the correct format";
-    }
-    // Send user to error page
-    res.render('sprint19/opt1/error-NINO-number', { "errMsg": errMsg });
-  }
-})
-
-// this is for NINO number for UR round-4 (option-2)
-router.post('/sprint19/opt2/engagement-type', function (req, res) {
-
-  req.session.data['What-type-of-engagement-is-it'] = '';
-  req.session.data['Who-is-the-engagement-with'] = '';
-  req.session.data['Do-you-want-to-complete-the-session'] = '';
-  req.session.data['What-services-have-they-called-about'] = '';
-  req.session.data['esaPayment'] = '';
-  req.session.data['pipPayment'] = '';
-  req.session.data['caPayment'] = '';
-  req.session.data['ca-payment'] = '';
-  req.session.data['esa-payment'] = '';
-  req.session.data['pip-payment'] = '';
-  req.session.data['esa-process-chasing'] = '';
-  req.session.data['pip-process-chasing'] = '';
-  req.session.data['ca-process-chasing'] = '';
-  req.session.data['esa-coc'] = '';
-  req.session.data['pip-coc'] = '';
-  req.session.data['ca-coc'] = '';
-  req.session.data['esa-general'] = '';
-  req.session.data['pip-general'] = '';
-  req.session.data['ca-general'] = '';
-
-  if (req.session.data['national-insurance-number-for-ur4_2'] == 'QQ123456Q' || req.session.data['national-insurance-number-for-ur4_2'] == 'qq123456q') {
-    // Send user to next page
-    res.render('sprint19/opt2/engagement-type')
-  } else {
-    var errMsg = "";
-    if (req.session.data['national-insurance-number-for-ur4_2'] == '' || req.session.data['national-insurance-number-for-ur4_2'] == undefined) {
-      errMsg = "National Insurance Number is mandatory";
-    } else {
-      errMsg = "Enter a National Insurance number in the correct format";
-    }
-    // Send user to error page
-    res.render('sprint19/opt2/error-NINO-number', { "errMsg": errMsg });
-  }
-})
 
 
 
@@ -4130,77 +4055,6 @@ router.get('/sprint13/opt2/engagement-log-journey/add-note/:name', function (req
   });
 });
 
-// this is for NINO number for UR round-5
-router.post('/sprint22/opt1/engagement-type', function (req, res) {
-
-  req.session.data['What-type-of-engagement-is-it-ur5'] = '';
-  req.session.data['Who-is-the-engagement-with-ur5'] = '';
-  req.session.data['govuk-hint govuk-radios__hint'] = '';
-  req.session.data['Do-you-want-to-complete-the-session'] = '';
-  req.session.data['What-services-have-they-called-about'] = '';
-  req.session.data['esaPayment'] = '';
-  req.session.data['pipPayment'] = '';
-  req.session.data['caPayment'] = '';
-  req.session.data['ca-payment'] = '';
-  req.session.data['esa-payment'] = '';
-  req.session.data['pip-payment'] = '';
-  req.session.data['esa-process-chasing'] = '';
-  req.session.data['pip-process-chasing'] = '';
-  req.session.data['ca-process-chasing'] = '';
-  req.session.data['esa-coc'] = '';
-  req.session.data['pip-coc'] = '';
-  req.session.data['ca-coc'] = '';
-  req.session.data['esa-general'] = '';
-  req.session.data['pip-general'] = '';
-  req.session.data['ca-general'] = '';
-
-  if (req.session.data['national-insurance-number-for-ur5'] == 'QQ123456Q' || req.session.data['national-insurance-number-for-ur5'] == 'qq123456q') {
-    // Send user to next page
-    res.render('sprint22/opt1/engagement-type')
-  } else {
-    var errMsg = "";
-    if (req.session.data['national-insurance-number-for-ur5'] == '' || req.session.data['national-insurance-number-for-ur5'] == undefined) {
-      errMsg = "National Insurance Number is mandatory";
-    } else {
-      errMsg = "Enter a National Insurance number in the correct format";
-    }
-    // Send user to error page
-    res.render('sprint22/opt1/error-NINO-number', { "errMsg": errMsg });
-  }
-})
-
-// this is for radio selection
-router.post('/sprint22/opt1/engagement-with', function (req, res) {
-  req.session.data['Who-is-the-engagement-with-ur5'] = '';
-  // Make a variable and give it the value from 'how-many-balls'
-  var howManyBalls = req.session.data['What-type-of-engagement-is-it-ur5'];
-  // Check whether the variable matches a condition
-  if (howManyBalls == "Incoming telephone call" || howManyBalls == "Outgoing telephone call") {
-    // Send user to next page
-    res.redirect('/sprint22/opt1/engagement-with');
-  } else {
-    req.session.data['What-type-of-engagement-is-it-ur5'] = '';
-    // Send user back to same page
-    res.redirect('back');
-  }
-
-})
-
-// this is for engagement with radio selection
-router.post('/sprint22/opt1/home-page-first-time', function (req, res) {
-
-  // Make a variable and give it the value from 'how-many-balls'
-  var engagemetWith = req.session.data['Who-is-the-engagement-with-ur5'];
-  // Check whether the variable matches a condition
-  if (engagemetWith == "Not Listed above" || engagemetWith == "") {
-    // Send user back to same page
-    res.redirect('back');
-  } else {
-    // Send user to next page
-    res.redirect('/sprint22/opt1/home-page-first-time');
-  }
-
-})
 
 
 router.post('/prototype-sprint-wise/sprint22/opt1/engagement-log-journey/what-service-called-about', function (req, res) {
@@ -10609,6 +10463,8 @@ router.post('/prototype-dev-baseline/mvp-1_0/different-type-contact-user/goToHom
 
 // Start folder specific routes
 
+// sprint mvp1.1, remember to add older sprint when adding a new folder!
+router.use('/prototype-sprint-wise/ur-5', require('./views/prototype-sprint-wise/ur-5/_routes'));
 // sprint mvp1.1, remember to add older sprint when adding a new folder!
 router.use('/prototype-dev-baseline/mvp-1_1', require('./views/prototype-dev-baseline/mvp-1_1/_routes'));
 // sprint ur-8, remember to add older sprint when adding a new folder!
